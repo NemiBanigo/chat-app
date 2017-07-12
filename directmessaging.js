b15b61a0440b15a381caf82409ab0ebed78f0878
Nemi <nemibanigo@gmail.com>
2017-07-12 16:52:27 +0100

    Added files to repo

---
+++ directmessaging.js
@@ -0,0 +1,96 @@
+	<script>
+ 	// const name = prompt('what is your name, soldier?')
+ 	
+
+ 	 	// if (name == 'nemi'){
+ 	 	// 	alert ('Oh hi there you bombass developer ' + name + '!');
+ 	 	// }
+ 	 	// else if (name == 'holly') {
+ 	 	// 	alert(`What is it you're doing,`
+ 	 	// 	 + name +'?');
+ 	 	// }
+ 	 	// else {
+ 	 	// 	console.log('try again, sweetheart')
+ 	 	// }
+ 	 	
+
+	// Comparisons
+ 	// const firstArr = [1,2,3];
+ 	// const secondArr = [1,2,3];
+ 	// const firstObj ={ color: 'red'};
+ 	// const secondObj = { color: 'red'};
+ 	// const otis = 'blah';
+
+ 	// 	console.log(Boolean('') && Boolean('')); 	
+ 	// const name = prompt ('what is your name?')	;
+ 	// const username = 'nemicoding';
+ 	// if (name == 'nemi') {
+ 	// 	alert('hello you bombass developer, ' + name + ' !') ;
+ 	// 	}else if ( name == 'ugo'){
+ 	// 		alert('hey there mr toptal')
+ 	// 	}else{
+ 	// 		console.log('not a developer, sorry.')
+ 	// 	}
+ 	
+ 	// const dogsList = document.querySelector('.dogs-list');
+ 	// const dogs = [
+	 // 	{ name: 'bruce' , type: 'chihuahua' },
+	 // 	{ name: 'chance' , type: 'bloodhound' }
+ 	// ];
+
+ 	// FOR LOOP
+ 	// const dogsList = document.querySelector('.dogs-list');
+ 	// const dogs = [
+ 	// 	{ name: 'Bruce' , type: 'Chihuahua' },
+ 	// 	{ name: 'Tualib', type: ' Bloodhound'},
+ 	// 	{ name: 'Takkendazz', type: ' Pomeranian'}
+ 	// ];
+ 	// for (let i=0; i< dogs.length; i++){
+ 	// 	//grab the dog, so we create new const
+ 	// 	const dog = dogs[i];
+ 	// 	const dogInfo =document.createElement('h1');
+ 	// 	dogInfo.classList.add('jumbotron', 'text-center');
+ 	// 	dogInfo.innerText = `${dog.name} is a ${dog.type}`;
+ 	// 	dogsList.appendChild(dogInfo);
+
+ 	// }
+ 	
+
+	 
+	 // }
+	 //WHILE LOOP
+		 // let i = 0;
+		 // while(i < dogs.length)	{
+		 // 	const dog 			= dogs[i];
+
+		 // 	const dogData 		= document.createElement('div');
+		 // 	dogData.classList.add('jumbotron','text-center')
+		 // 	dogData.innerText	 = `${dog.name} is a ${dog.type}`;
+		 // 	dogsList.appendChild(dogData); 
+		 // 	i++ 
+		 // }
+
+	// DO WHILE 	
+		 // let i = 0;
+		 // do {
+		 // 	//grab ze dog
+		 // 	const dog 			= dogs[i];
+
+		 // 	const dogData 		= document.createElement('div');
+		 // 	dogData.classList.add('jumbotron','text-center')
+		 // 	dogData.innerText	 = `${dog.name} is a ${dog.type}`;
+		 // 	dogsList.appendChild(dogData); 
+		 // 	i++ 
+		 // }	while (i < dogs.length);
+
+	//FOR ...OF ITERATOR
+
+		// for (let dog of dogs){
+		// 	const dogData = document.createElement('div');
+		// 	dogData.classList.add('jumbotron','text-center');
+		// 	dogData.innerText = `${dog.name} is a ${dog.type}`;
+		// 	dogsList.appendChild(dogData);
+		// }
+
+
+ 	</script>
